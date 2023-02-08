2266a66bc65c3e6f969c857fa8381bd879ab8ba1
WalterRiveraGT <walterej@hotmail.es>
2023-02-08 02:37:58 -0600

    agregando solución perteneciente al ejercicio 0 de literales y concatenación

--- 04-ejercicio concatenación y literales.sql
+++ 04-ejercicio concatenación y literales.sql
@@ -14,3 +14,19 @@ SELECT
     *
 FROM
     EMPLOYEES;
+
+SELECT
+    *
+FROM
+    LOCATIONS;
+
+/* EJERCICIO 02: 
+CREAR LA SIGUIENTE CONSULTA:
+EJ:
+CALLE Y CIUDAD
+--------------------------------------------------------------------------------------------------------------------------------
+LA CALLE 1297 VIA COLA DI RIE PERTENECE A LA CIUDAD: ROMA*/
+SELECT
+    'LA CALLE '||LOCATION_ID||' '||STREET_ADDRESS||' PERTENECE A LA CIUDAD: '||' '||STATE_PROVINCE AS "CALLE Y CIUDAD"
+FROM
+    LOCATIONS;
