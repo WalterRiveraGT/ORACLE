5910bc9846e68b2993eafeeaa5d3c8e98adf3942
WalterRiveraGT <walterej@hotmail.es>
2023-02-08 02:20:11 -0600

    agregando archivo para repaso de concatenación y literales

---
+++ 03-CONCATENACION Y LITERALES.sql
@@ -0,0 +1,9 @@
+/*video 44 literales*/
+/*poner una cadena de caracteres (sin concatenar) a un campo o varios*/
+SELECT
+    'NOMBRE:',
+    FIRST_NAME
+FROM
+    EMPLOYEES;
+
+/*concatenar CADENA DE CARACTERES*/
