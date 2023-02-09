e92915d8d13706757a17821d8a257c163b969e38
WalterRiveraGT <walterej@hotmail.es>
2023-02-08 21:57:43 -0600

    comparar fechas

--- 06 WHERE.sql
+++ 06 WHERE.sql
@@ -39,3 +39,20 @@ FROM
     EMPLOYEES
 WHERE
     FIRST_NAME='John';
+
+/*comparar fechas
+las fechas se guardan como numeros julianos
+la fecha tiene un número específico*/
+/*seleccionar aquellos empleados que entraron Entre
+el año 2006 y 2007*/
+SELECT
+    EMPLOYEE_ID,
+    FIRST_NAME,
+    LAST_NAME,
+    TO_DATE(HIRE_DATE,
+    'DD/MM/YYYY') AS FECHA_INGRESO
+FROM
+    EMPLOYEES
+WHERE
+    EXTRACT(YEAR FROM HIRE_DATE) BETWEEN 2006
+    AND 2007;
